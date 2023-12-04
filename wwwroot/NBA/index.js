c055de93ad8c6665b6e632372c0709c94e1897e2
JPBagagem <zpbagagem@gmail.com>
2023-12-04 13:59:52 +0000

    updateswitchbutton

--- wwwroot/NBA/index.js
+++ wwwroot/NBA/index.js
@@ -1,14 +1,18 @@
 document.getElementById('btnSwitch').addEventListener('click',()=>{
-    let navbar = document.getElementById("navtop");
+    let cabecalho = document.getElementById("navtop");
+    let rodape = document.getElementById("navbutton");
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
         document.documentElement.setAttribute('data-bs-theme','light')
-        document.documentElement.setAttribute('data-bs-theme','light')
-        navbar.classList.add("bluenav");
-        navbar.classList.remove("rednav");
+        cabecalho.classList.add("bluenav");
+        cabecalho.classList.remove("rednav");
+        rodape.classList.add("rednav");
+        rodape.classList.remove("bluenav");
     }
     else {
         document.documentElement.setAttribute('data-bs-theme','dark')
-        navbar.classList.add("rednav");
-        navbar.classList.remove("bluenav");
+        cabecalho.classList.add("rednav");
+        cabecalho.classList.remove("bluenav");
+        rodape.classList.add("bluenav");
+        rodape.classList.remove("rednav");
     }
 })
