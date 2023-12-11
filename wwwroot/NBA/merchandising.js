88faae0231613f3ef6cf6849fde6a7a645fa8c80
lopes04 <henriquemqlopes@gmail.com>
2023-12-11 16:28:22 +0000

    merchandising

--- wwwroot/NBA/merchandising.js
+++ wwwroot/NBA/merchandising.js
@@ -16,39 +16,39 @@ closeShopping.addEventListener('click', ()=>{
 let products = [
     {
         id: 1,
-        name: 'PRODUCT NAME 1',
-        image: '1.PNG',
-        price: 120000
+        name: 'Camisola Lakers',
+        image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/d9aa1b6a-4385-4233-96b6-b03f5c83d1a6/los-angeles-lakers-icon-edition-2022-23-dri-fit-nba-swingman-jersey-v5RTGr.png',
+        price: 69.99
     },
     {
         id: 2,
-        name: 'PRODUCT NAME 2',
-        image: '2.PNG',
-        price: 120000,
+        name: 'Camisola Spurs',
+        image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f6610b90-0526-44ca-a727-a9d727df98bd/san-antonio-spurs-icon-edition-2022-23-dri-fit-nba-swingman-jersey-p4XQ6b.png',
+        price: 69.99
     },
     {
         id: 3,
-        name: 'PRODUCT NAME 3',
-        image: '3.PNG',
-        price: 220000
+        name: 'Camisola Bulls',
+        image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/7c624765-1c34-4290-885c-ba692cadaa4e/chicago-bulls-icon-edition-2022-23-dri-fit-nba-swingman-jersey-xVTBkg.png',
+        price: 69.99
     },
     {
         id: 4,
-        name: 'PRODUCT NAME 4',
-        image: '4.PNG',
-        price: 123000
+        name: 'Camisola Golden State Warriors',
+        image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/7ac6733f-1dc1-46ce-ae77-fbd4c73099f9/camisola-nba-swingman-dri-fit-golden-state-warriors-icon-edition-2022-23-pZX5TF.png',
+        price: 69.99
     },
     {
         id: 5,
-        name: 'PRODUCT NAME 5',
-        image: '5.PNG',
-        price: 320000
+        name: 'Camisola Raptors',
+        image: 'https://images.footballfanatics.com/toronto-raptors/toronto-raptors-tracy-mcgrady-hardwood-classics-road-swingman-jerse-by-mitchell-and-ness-mens_ss4_p-11889789+pv-1+u-fr6tfjy873oqy5fne632+v-e39f16f44abd4d6299983220f09b99a8.jpg?_hv=2&w=600',
+        price: 69.99
     },
     {
         id: 6,
-        name: 'PRODUCT NAME 6',
-        image: '6.PNG',
-        price: 120000
+        name: 'Camisola Rockets',
+        image: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/6fd252da-f4c6-4378-8523-6ca6e9e66004/houston-rockets-icon-edition-2022-23-dri-fit-nba-swingman-jersey-LQh428.png',
+        price: 69.99
     }
 ];
 let listCards  = [];
@@ -57,7 +57,7 @@ function initApp(){
         let newDiv = document.createElement('div');
         newDiv.classList.add('item');
         newDiv.innerHTML = `
-            <img src="image/${value.image}">
+            <img src="${value.image}">
             <div class="title">${value.name}</div>
             <div class="price">${value.price.toLocaleString()}</div>
             <button onclick="addToCard(${key})">Adicionar ao carrinho</button>`;
@@ -83,7 +83,7 @@ function reloadCard(){
         if(value != null){
             let newDiv = document.createElement('li');
             newDiv.innerHTML = `
-                <div><img src="image/${value.image}"/></div>
+                <div><img src="${value.image}"/></div>
                 <div>${value.name}</div>
                 <div>${value.price.toLocaleString()}</div>
                 <div>
