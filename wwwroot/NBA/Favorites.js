c688ea6e5b66baf45ed0a7ed7b0dd6d9f0737263
JPBagagem <zpbagagem@gmail.com>
2023-12-16 22:56:02 +0000

    Henrique vou mexer na navbar agora, não lhe toques

---
+++ wwwroot/NBA/Favorites.js
@@ -0,0 +1,97 @@
+﻿document.getElementById('btnSwitch').addEventListener('click',()=>{
+    let cabecalho = document.getElementById("navtop");
+    let rodape = document.getElementById("navbutton");
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+        cabecalho.classList.add("bluenav");
+        cabecalho.classList.remove("rednav");
+        rodape.classList.add("rednav");
+        rodape.classList.remove("bluenav");
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+        cabecalho.classList.add("rednav");
+        cabecalho.classList.remove("bluenav");
+        rodape.classList.add("bluenav");
+        rodape.classList.remove("rednav");
+    }
+})
+
+// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    self.displayName = 'NBA Players List';
+    self.updateLocalStorage = (key, data) => {
+        localStorage.setItem(key, JSON.stringify(data))
+    }
+    self.SetFavourites = ko.observableArray(JSON.parse(localStorage.getItem("playerFavorites")));
+    self.favButton = (_data,_event) =>{
+        console.log(_event);
+        if (_data.target.classList.contains('fa-heart-o')){
+            self.SetFavourites.push(_event)
+            self.updateLocalStorage("playerFavorites", self.SetFavourites())
+            _data.target.classList.remove('fa-heart-o');
+            _data.target.classList.add('fa-heart');
+        }
+        else{
+            for (let i = 0; i < self.SetFavourites().length; i++) {
+                console.log(i)
+                if (self.SetFavourites()[i].Id== _event.Id){
+                    console.log(i)
+                    self.SetFavourites.splice(i,1)
+                    self.updateLocalStorage("playerFavorites", self.SetFavourites())
+                    _data.target.classList.remove('fa-heart');
+                    _data.target.classList.add('fa-heart-o');
+                }
+            }
+        }
+        console.log(self.SetFavourites());
+    }
+
+    function sleep(milliseconds) {
+        const start = Date.now();
+        while (Date.now() - start < milliseconds);
+    }
+
+    function showLoading() {
+        $("#myModal").modal('show', {
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+    //--- start ....
+    showLoading();
+    console.log("VM initialized!");
+    hideLoading();
+};
+
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
+});
+
+function tabelas(){
+    let tinicial=document.getElementById("inicial");
+    let tfotos=document.getElementById("foto");
+    let button=document.getElementById("btn-table");
+    if (tinicial.classList.contains("d-none")) {
+        tfotos.classList.add("d-none");
+        tinicial.classList.remove("d-none");
+        button.classList.add("fa-picture-o");
+        button.classList.remove("fa-table");
+    }
+    else {
+        tfotos.classList.remove("d-none");
+        tinicial.classList.add("d-none");
+        button.classList.remove("fa-picture-o");
+        button.classList.add("fa-table");
+    }
+}
