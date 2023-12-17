d04e8040fb705b0b73614dd573afe9838650e388
JPBagagem <zpbagagem@gmail.com>
2023-12-17 01:48:09 +0000

    Players já mantêm o darkmode

--- wwwroot/NBA/players.js
+++ wwwroot/NBA/players.js
@@ -1,23 +1,4 @@
-﻿document.getElementById('btnSwitch').addEventListener('click',()=>{
-    let cabecalho = document.getElementById("navtop");
-    let rodape = document.getElementById("navbutton");
-    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-        document.documentElement.setAttribute('data-bs-theme','light')
-        cabecalho.classList.add("bluenav");
-        cabecalho.classList.remove("rednav");
-        rodape.classList.add("rednav");
-        rodape.classList.remove("bluenav");
-    }
-    else {
-        document.documentElement.setAttribute('data-bs-theme','dark')
-        cabecalho.classList.add("rednav");
-        cabecalho.classList.remove("bluenav");
-        rodape.classList.add("bluenav");
-        rodape.classList.remove("rednav");
-    }
-})
-
-// ViewModel KnockOut
+﻿// ViewModel KnockOut
 var vm = function () {
     console.log('ViewModel initiated...');
     //---Variáveis locais
@@ -31,6 +12,7 @@ var vm = function () {
         localStorage.setItem(key, JSON.stringify(data))
     }
     self.SetFavourites = ko.observableArray(JSON.parse(localStorage.getItem("playerFavorites")));
+    self.darktolight = ko.observable(JSON.parse(localStorage.getItem("darktolight")))
     self.currentPage = ko.observable(1);
     self.pagesize = ko.observable(20);
     self.totalRecords = ko.observable(50);
@@ -74,7 +56,18 @@ var vm = function () {
             $('#fav'+ gostos[i].Id).removeClass('fa-heart-o');
         }
     }
-
+    self.darkSetup = function(){
+        let cabecalho = document.getElementById("navtop");
+        let rodape = document.getElementById("navbutton");
+        console.log(self.darktolight())
+        if (self.darktolight() == 'dark') {
+            document.documentElement.setAttribute('data-bs-theme','dark')
+            cabecalho.classList.remove("bluenav");
+            cabecalho.classList.add("rednav");
+            rodape.classList.remove("rednav");
+            rodape.classList.add("bluenav");
+        }
+    }
     self.search = function() { 
         console.log("searching")
         if ($("#searchbar").val() === "") {
@@ -151,6 +144,7 @@ var vm = function () {
             self.totalPages(data.TotalPages);
             self.totalRecords(data.TotalRecords);
             self.SetupFavourites();
+            self.darkSetup();
         });
     };
 
@@ -171,7 +165,31 @@ var vm = function () {
             }
         });
     }
-    
+
+    $('#btnSwitch').click(function(){
+        let cabecalho = document.getElementById("navtop");
+        let rodape = document.getElementById("navbutton");
+        console.log(self.darktolight())
+        if (self.darktolight() == 'dark') {
+            self.darktolight('light')
+            self.updateLocalStorage("darktolight", 'light')
+            document.documentElement.setAttribute('data-bs-theme','light')
+            cabecalho.classList.add("bluenav");
+            cabecalho.classList.remove("rednav");
+            rodape.classList.add("rednav");
+            rodape.classList.remove("bluenav");
+        }
+        else {
+            self.darktolight('dark')
+            self.updateLocalStorage("darktolight", 'dark')
+            document.documentElement.setAttribute('data-bs-theme','dark')
+            cabecalho.classList.add("rednav");
+            cabecalho.classList.remove("bluenav");
+            rodape.classList.add("bluenav");
+            rodape.classList.remove("rednav");
+        }
+    })
+
     self.favButton = (_data,_event) =>{
         console.log(_event);
         if (_data.target.classList.contains('fa-heart-o')){
