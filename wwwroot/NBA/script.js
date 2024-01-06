36f34442c7b5a1cfe5358717da0bef105adb8357
JPBagagem <zpbagagem@gmail.com>
2024-01-06 13:45:19 +0000

    i forgur disto

--- wwwroot/NBA/script.js
+++ wwwroot/NBA/script.js
@@ -3,6 +3,7 @@ var darkbutton = document.getElementById("btnSwitch")
 function darkSetup() {
     let vermelhos = document.querySelectorAll(".rednav");
     let azuis = document.querySelectorAll(".bluenav");
+    let letra_preta = document.querySelectorAll(".black-text")
     document.documentElement.setAttribute('data-bs-theme','dark')
     for(i=0; i < vermelhos.length;i++){
         vermelhos[i].classList.remove("rednav");
@@ -11,6 +12,10 @@ function darkSetup() {
     for(i=0; i < azuis.length;i++){
         azuis[i].classList.remove("bluenav");
         azuis[i].classList.add("rednav");
+    }
+    for(i=0; i < letra_preta.length;i++){
+        letra_preta[i].classList.remove("black-text");
+        letra_preta[i].classList.add("white-text");
     }
 }
 
@@ -23,11 +28,22 @@ darkbutton.addEventListener("click", function(){
         darktolight='light'
         localStorage.setItem("darktolight", JSON.stringify('light'))
         document.documentElement.setAttribute('data-bs-theme','light')
+        let letra_branca = document.querySelectorAll(".white-text")
+        for(i=0; i < letra_branca.length;i++){
+            letra_branca[i].classList.remove("white-text");
+            letra_branca[i].classList.add("black-text");
+        }
     }
     else {
         darktolight='dark'
         localStorage.setItem("darktolight", JSON.stringify('dark'))
         document.documentElement.setAttribute('data-bs-theme','dark')
+        let letra_preta = document.querySelectorAll(".black-text")
+        for(i=0; i < letra_preta.length;i++){
+            letra_preta[i].classList.remove("black-text");
+            letra_preta[i].classList.add("white-text");
+        }
+
     }
     for(i=0; i < vermelhos.length;i++){
         vermelhos[i].classList.remove("rednav");
